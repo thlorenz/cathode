3e7a6804a1d8206c201a9a62bcde052f0328f94f
Thorsten Lorenz <thlorenz@gmx.de>
2016-12-08 21:26:22 -0800

    removing no longer needed index page

--- src/server.js
+++ src/server.js
@@ -9,10 +9,6 @@ const staticPath = path.join(__dirname, '..', 'static')
 app.use('/static', express.static(staticPath))
 app.get('*', require('./render.jsx'))
 
-app.get('/', function serverRoot(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
-
 app.listen(3000, function onlistening() {
   console.log('Example app listening on port 3000!')
 })
